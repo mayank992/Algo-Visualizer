eff0ae7e066ba3ef18138b1b9438a04e1f344ddf
mayank992 <mayankjindal992@gmail.com>
2020-08-19 04:04:23 +0530

    update

--- Script/script.js
+++ Script/script.js
@@ -1,22 +1,20 @@
-var arr=[65,87,67,70,56,90,54,69,60,89,87,67,70,56,61,76,45,91,59,78,92,51,43,76,90];
-
 let ctx = document.getElementById('myChart').getContext('2d');
 let chart = new Chart(ctx, {
-    type: 'horizontalBar',
+    type: 'bar',
     data: {
-        labels: ['A[0]', 'A[1]', 'A[2]', 'A[3]', 'A[4]', 'A[5]', 'A[6]', 'A[7]', 'A[8]', 'A[9]', 'A[10]', 'A[11]', 'A[12]', 'A[13]', 'A[14]', 'A[15]', 'A[16]', 'A[17]', 'A[18]', 'A[19]', 'A[20]', 'A[21]', 'A[22]', 'A[23]', 'A[24]'],
+        labels: [],
         datasets: [{
-            label: 'Element',
-            data: arr,
-            backgroundColor: ['blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue'],
+            label: '',
+            data: [],
+            backgroundColor: [],
             borderColor: 'black', 
-            hoverBackgroundColor: 'red',
-            borderWidth: 0
+            hoverBackgroundColor: 'black',
         }]
     },
+
     options: {
         animation: {
-            duration: 0.5,
+            duration: 100
         }, 
         scales: {
             xAxes: [{
@@ -25,7 +23,8 @@ let chart = new Chart(ctx, {
                     display:false
                 },
                 ticks: {
-                    display: false
+                    display: false,
+                    min: 0
                 }
             }],
             yAxes: [{
@@ -34,74 +33,162 @@ let chart = new Chart(ctx, {
                     display:false
                 }, 
                 ticks: {
-                    min: 0,
+                    display: false,
+                    min: 0
                 } 
             }]
+        },
+        options: {
+            
         }
     }
 });
 
-let n = 25, speed = 1;
+let n = 100, speed = 1;
 let dataset = chart.data.datasets[0];
-let slider = document.getElementById('speed');
+let slider = document.getElementById("myRange");
 slider.oninput = function() {
     speed = this.value;
 }
 
-function updateChartColor(color, index, delay) {
-    setTimeout(function() {
-        dataset.backgroundColor[index] = color;
-        chart.update();
-    }, delay);
+// initialize the dataset of the chart object(Using random function
+// to generate random values)
+function init(){
+    for (let i = 0; i < 70; i++) {
+        chart.data.labels.push('Arr');
+        dataset.backgroundColor.push('blue');
+        dataset.data.push(Math.ceil(Math.random() * 133));
+    }
+    n = dataset.data.length;
+    chart.update();
 }
 
-function compare(i, j) {
-    setTimeout(function() {
-        if(dataset.data[j] > dataset.data[j+1]) {
-            let temp = dataset.data[j];
-            dataset.data[j] = dataset.data[j+1];
-            dataset.data[j+1] = temp;
-            updateChartColor('red', j, 0);
-            updateChartColor('red', j+1, 0);
+// calling init function during loading of webpage
+init();
+
+// refresh the whole webpage
+function reset() {
+    location.reload();
+}
+
+// sleep for ms millisecond 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// update color of chart bar at index idx with delay
+async function updateBarColor(color, idx, delay) {
+    dataset.backgroundColor[idx] = color;
+    chart.update();
+    await sleep(delay);
+}
+
+// swap the values present at index i & j with delay
+async function swap(i, j, delay) {
+    let temp = dataset.data[i];
+    dataset.data[i] = dataset.data[j];
+    dataset.data[j] = temp;
+    await sleep(delay);
+    chart.update();
+}
+
+// Bubble Sort Algorithm 
+// Time Complexity: O(N^2)
+async function BubbleSort() {
+    console.log(speed);
+    // console.log('In Bubble Sort.');
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            await updateBarColor('black', j, 0);
+            await updateBarColor('black', j + 1, 1000 / speed);
+            if (dataset.data[j] > dataset.data[j + 1]) {
+                await updateBarColor('red', j, 0);
+                await updateBarColor('red', j + 1, 1000 / speed);
+                await swap(j, j + 1, 0);
+            }
+            await updateBarColor('green', j, 0);
+            await updateBarColor('green', j + 1, 1000 / speed);
+            await updateBarColor('blue', j, 0);
+            await updateBarColor('blue', j + 1, 0);
         }
-        else {
-            updateChartColor('green', j, 0);
-            updateChartColor('green', j+1, 0);
-        }
+        await updateBarColor('purple', n - i - 1, 1000 / speed);
+    }
+}
+
+async function updateMergedResult(result, low, high) {
+    for (i = low; i <= high; i++) {
+        dataset.data[i] = result[i - low];
         chart.update();
-        // console output for testing
-        console.log('In Compare Function');
-    }, 1000 / speed);
+        await sleep(1000 / speed);
+    }
 }
 
-function toloop(i, j) {
-    setTimeout(function () {
-        updateChartColor('black', j, 0);
-        updateChartColor('black', j+1, 0);
-        compare(i, j);
-        updateChartColor('blue', j, 2000 / speed);    
-        updateChartColor('blue', j+1, 2000 / speed)
-        // console output for testing
-        console.log("In Inside Loop" + j);
-        if(++j < n-i-1)
-            toloop(i, j);
-    }, 3000 / speed);
+async function merge(low, mid, high) {
+    i = low, j = mid + 1;
+    result = [];
+    while (i <= mid && j <= high) {
+        if (dataset.data[i] <= dataset.data[j])
+            result.push(dataset.data[i++]);
+        else 
+            result.push(dataset.data[j++]);
+    }
+    while (i <= mid) {
+        result.push(dataset.data[i++]);
+    }
+    while (j <= high) {
+        result.push(dataset.data[j++]);
+    }
+    await updateMergedResult(result, low, high);
 }
 
-function loop(i) {
-    setTimeout(function () {
-        // console output for testing 
-        console.log("In Outside Loop: " + i);
-        toloop(i, 0);
-        // update the color of already sorted bar
-        updateChartColor('purple', n-i-1, (3000 * (n - i)) / speed);
-        if(i == n-2)
-            updateChartColor('purple', 0, (3000 * (n - i)) / speed);
-        if(++i < n-1)
-            loop(i);
-    }, (i == 0)? 0: (3000 * (n - i)) / speed);
+async function mergeSortHelper(low, high) {
+    if (low >= high) return ;
+    const mid = Math.floor((low + high) / 2);
+    await mergeSortHelper(low, mid);
+    await mergeSortHelper(mid + 1, high);
+    await merge(low, mid, high);
 }
 
+// Merge Sort Algorithm
+// Time Complexity: O(N logN)
+function MergeSort() {
+    //console.log('In MergeSort.');
+    mergeSortHelper(0, n - 1);
+}
+
+// Quick Sort Algorithm
+// Time Complexity: Average O(N logN), Worst O(N^2)
+function QuickSort() {
+    console.log('In QuickSort.');
+}
+
+// Haep Sort Algorithm
+// Time Complexity: O(N logN)
+function HeapSort() {
+    console.log('In HeapSort.');
+}
+
+// on clicking the sort button this function checks all the values of 
+// the radio button and call the approriate sorting algorithm function 
 function sort() {
-    loop(0);
+    let toCall = 4;
+    let option = document.getElementsByName('choice');
+    for (i = 0; i < option.length; i++) {
+        if (option[i].checked) {
+            toCall = i;
+            break;
+        }
+    }
+    switch(toCall) {
+        case 0: BubbleSort();
+            break;
+        case 1: MergeSort();
+            break;
+        case 2: QuickSort();
+            break;
+        case 3: HeapSort();
+            break;
+        default:
+            alert('Please select the Sorting Algorithm.');
+    }
 }

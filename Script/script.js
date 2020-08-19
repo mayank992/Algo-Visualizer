a2d6c26781b7a15cf541efbd73e30e540b96bc57
mayank992 <mayankjindal992@gmail.com>
2020-08-20 02:39:09 +0530

    update

--- Script/script.js
+++ Script/script.js
@@ -44,12 +44,16 @@ let chart = new Chart(ctx, {
     }
 });
 
+//###################################################################
+
 let n = 100, speed = 1;
 let dataset = chart.data.datasets[0];
 let slider = document.getElementById("myRange");
 slider.oninput = function() {
     speed = this.value;
 }
+
+//###################################################################
 
 // initialize the dataset of the chart object(Using random function
 // to generate random values)
@@ -63,24 +67,20 @@ function init(){
     chart.update();
 }
 
+// calling init function during loading of webpage
+init();
+
 function reset() {
     chart.data.labels = []
     chart.backgroundColor = [];
     dataset.data = [];
+    slider.value = 1;
     let option = document.getElementsByName('choice');
     for (i = 0; i < option.length; i++) {
         option[i].checked = false;
     }
     init();
 }
-
-// calling init function during loading of webpage
-init();
-
-// refresh the whole webpage
-// function reset() {
-//     location.reload();
-// }
 
 // sleep for ms millisecond 
 function sleep(ms) {
@@ -103,6 +103,8 @@ async function swap(i, j, delay) {
     chart.update();
 }
 
+//###################################################################
+
 // Bubble Sort Algorithm 
 // Time Complexity: O(N^2)
 async function BubbleSort() {
@@ -124,32 +126,41 @@ async function BubbleSort() {
         }
         await updateBarColor('purple', n - i - 1, 1000 / speed);
     }
-}
-
-async function updateMergedResult(result, low, high) {
-    for (i = low; i <= high; i++) {
-        dataset.data[i] = result[i - low];
-        chart.update();
-        await sleep(1000 / speed);
+    for (i = 0; i < n; i++) {
+        updateBarColor('green', i, 0);
     }
 }
+
+//###################################################################
 
 async function merge(low, mid, high) {
     i = low, j = mid + 1;
-    result = [];
-    while (i <= mid && j <= high) {
-        if (dataset.data[i] <= dataset.data[j])
-            result.push(dataset.data[i++]);
-        else 
-            result.push(dataset.data[j++]);
+    while (i <= high && j <= high) {
+        await updateBarColor('black', i, 0);
+        await updateBarColor('black', j, 1000 / speed);
+        if (dataset.data[i] > dataset.data[j]) {
+            await updateBarColor('red', j, 1000 / speed);
+            const temp = dataset.data[j];
+            for (k = j - 1; k >= i; k--) {
+                dataset.data[k + 1] = dataset.data[k];
+            }
+            dataset.data[i] = temp;
+            chart.update();
+            await updateBarColor('black', j, 0);
+            await updateBarColor('green', i, 1000 / speed);
+            await updateBarColor('purple', j, 0);
+            j++;
+        }
+        else {
+            await updateBarColor('green', i, 1000 / speed);
+            await updateBarColor('purple', j, 0);
+        }
+        i++;
     }
-    while (i <= mid) {
-        result.push(dataset.data[i++]);
+    while (i <= high) {
+        await updateBarColor('green', i, 1000 / speed);
+        i++;
     }
-    while (j <= high) {
-        result.push(dataset.data[j++]);
-    }
-    await updateMergedResult(result, low, high);
 }
 
 async function mergeSortHelper(low, high) {
@@ -157,21 +168,58 @@ async function mergeSortHelper(low, high) {
     const mid = Math.floor((low + high) / 2);
     await mergeSortHelper(low, mid);
     await mergeSortHelper(mid + 1, high);
+    for (i = low; i <= high; i++) {
+        updateBarColor('purple', i, 0);
+    }
+    await sleep(1000 / speed);
     await merge(low, mid, high);
+    for (i = low; i <= high; i++) {
+        updateBarColor('blue', i, 0);
+    }
+    await sleep(1000 / speed);
 }
 
 // Merge Sort Algorithm
 // Time Complexity: O(N logN)
-function MergeSort() {
+async function MergeSort() {
     //console.log('In MergeSort.');
-    mergeSortHelper(0, n - 1);
+    await mergeSortHelper(0, n - 1);
+    for (i = 0; i < n; i++) {
+        updateBarColor('green', i, 0);
+    }
+}
+
+//###################################################################
+
+// partition the segment [low, high] into two parts
+// by placing the pivot element on itss corrent position
+async function partition(low, high) {
+    pivot = dataset.data[high];
+    i = low - 1, j = low;
+    while (j < high) {
+        if (dataset.data[j] < pivot) {
+            await swap(++i, j, 1000 / speed);
+        }
+        j++;
+    }
+    await swap(i + 1, high, 1000 / speed);
+    return i + 1;
+}
+
+async function QuickSortHelper(low, high) {
+    if (low >= high) return ;
+    const pivot = await partition(low, high);
+    await QuickSortHelper(low, pivot - 1);
+    await QuickSortHelper(pivot + 1, high);
 }
 
 // Quick Sort Algorithm
 // Time Complexity: Average O(N logN), Worst O(N^2)
 function QuickSort() {
-    console.log('In QuickSort.');
+    QuickSortHelper(0, n - 1);
 }
+
+//###################################################################
 
 // Haep Sort Algorithm
 // Time Complexity: O(N logN)
@@ -179,6 +227,7 @@ function HeapSort() {
     console.log('In HeapSort.');
 }
 
+//###################################################################
 // on clicking the sort button this function checks all the values of 
 // the radio button and call the approriate sorting algorithm function 
 function sort() {
